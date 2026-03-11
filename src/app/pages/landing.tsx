3ec2cdb8932166288ab565871bb5c93b412d1698
Niyati <niyatiagarwal2901@gmail.com>
2026-03-11 18:45:39 +0530

    Update landing.tsx

--- src/app/pages/landing.tsx
+++ src/app/pages/landing.tsx
@@ -1,33 +1,5 @@
-import { useNavigate } from "react-router-dom";
-import { generateBlueprint } from "../api/operai";
-import { useState } from "react";
-
 function Landing() {
-  const navigate = useNavigate();
-  const [idea, setIdea] = useState("");
-
-  const handleSubmit = async () => {
-    try {
-      const data = await generateBlueprint(idea, 0);
-
-      if (data.mode === "clarification") {
-        navigate("/clarification", { state: data });
-      }
-
-      if (data.mode === "execution") {
-        navigate("/blueprint", { state: data.human_readable });
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  return (
-    <div>
-      {/* your UI */}
-      <button onClick={handleSubmit}>Generate</button>
-    </div>
-  );
+  return <div style={{ padding: 40 }}>Landing Page Works</div>;
 }
 
 export default Landing;
