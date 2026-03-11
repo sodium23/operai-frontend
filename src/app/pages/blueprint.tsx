d697d1990e25f8314e3d83ae284374564fbe02b9
Niyati <niyatiagarwal2901@gmail.com>
2026-03-11 17:43:57 +0530

    Update blueprint.tsx

--- src/app/pages/blueprint.tsx
+++ src/app/pages/blueprint.tsx
@@ -1,16 +1,37 @@
 import { useLocation } from "react-router-dom";
+import IdeaInterpretation from "../components/blueprint/idea-interpretation";
+import MarketReality from "../components/blueprint/market-reality";
+import MoatAnalysis from "../components/blueprint/moat-analysis";
+import ConfidenceScore from "../components/blueprint/confidence-score";
+import ProductBlueprint from "../components/blueprint/product-blueprint";
+import PRDSection from "../components/blueprint/prd-section";
+import ArchitectureSection from "../components/blueprint/architecture-section";
+import SecuritySection from "../components/blueprint/security-section";
+import EdgeCasesSection from "../components/blueprint/edge-cases-section";
+import ValidationSection from "../components/blueprint/validation-section";
 
-const location = useLocation();
-const blueprint = location.state;
+function Blueprint() {
+  const location = useLocation();
+  const blueprint = location.state;
 
+  if (!blueprint) {
+    return <div>No blueprint data available</div>;
+  }
 
-<IdeaInterpretation data={blueprint.idea_interpretation} />
-<MarketReality data={blueprint.market_reality} />
-<MoatAnalysis data={blueprint.moat_analysis} />
-<ConfidenceScore data={blueprint.confidence_score} />
-<ProductBlueprint data={blueprint.product_blueprint} />
-<PRDSection data={blueprint.prd} />
-<ArchitectureSection data={blueprint.architecture} />
-<SecuritySection data={blueprint.security_governance} />
-<EdgeCasesSection data={blueprint.critical_edge_cases} />
-<ValidationSection data={blueprint.validation_plan} />
+  return (
+    <div className="p-8 space-y-8">
+      <IdeaInterpretation data={blueprint.idea_interpretation} />
+      <MarketReality data={blueprint.market_reality} />
+      <MoatAnalysis data={blueprint.moat_analysis} />
+      <ConfidenceScore data={blueprint.confidence_score} />
+      <ProductBlueprint data={blueprint.product_blueprint} />
+      <PRDSection data={blueprint.prd} />
+      <ArchitectureSection data={blueprint.architecture} />
+      <SecuritySection data={blueprint.security_governance} />
+      <EdgeCasesSection data={blueprint.critical_edge_cases} />
+      <ValidationSection data={blueprint.validation_plan} />
+    </div>
+  );
+}
+
+export default Blueprint;
