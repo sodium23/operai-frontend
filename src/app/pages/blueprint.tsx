7c2a91324a1264a34cf6a230eb21bc5196e9690f
Niyati <niyatiagarwal2901@gmail.com>
2026-03-11 15:53:08 +0530

    Add sections to blueprint component

--- src/app/pages/blueprint.tsx
+++ src/app/pages/blueprint.tsx
@@ -2,3 +2,15 @@ import { useLocation } from "react-router-dom";
 
 const location = useLocation();
 const blueprint = location.state;
+
+
+<IdeaInterpretation data={blueprint.idea_interpretation} />
+<MarketReality data={blueprint.market_reality} />
+<MoatAnalysis data={blueprint.moat_analysis} />
+<ConfidenceScore data={blueprint.confidence_score} />
+<ProductBlueprint data={blueprint.product_blueprint} />
+<PRDSection data={blueprint.prd} />
+<ArchitectureSection data={blueprint.architecture} />
+<SecuritySection data={blueprint.security_governance} />
+<EdgeCasesSection data={blueprint.critical_edge_cases} />
+<ValidationSection data={blueprint.validation_plan} />
