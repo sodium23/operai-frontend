1b83016f978699362dd588c910c49c379ddb7fee
Niyati <niyatiagarwal2901@gmail.com>
2026-03-11 17:38:05 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing";
+import Clarification from "./pages/clarification";
+import Blueprint from "./pages/blueprint";
 
 function App() {
-  return <Landing />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/clarification" element={<Clarification />} />
+        <Route path="/blueprint" element={<Blueprint />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
