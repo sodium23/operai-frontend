3c4a628cc907af0866110834ebff5567c0a3cb36
Niyati <niyatiagarwal2901@gmail.com>
2026-03-12 18:15:05 +0530

    Update blueprint.tsx

--- src/app/pages/blueprint.tsx
+++ src/app/pages/blueprint.tsx
@@ -88,23 +88,7 @@ export default function Blueprint() {
 
   <IdeaInterpretation data={blueprint.idea_interpretation} />
 
-      <MarketReality data={blueprint.market_reality} />
 
-      <MoatAnalysis data={blueprint.moat_analysis} />
-
-      <ConfidenceScore data={blueprint.confidence_score} />
-
-      <ProductBlueprint data={blueprint.product_blueprint} />
-
-      <PRDSection data={blueprint.prd} />
-
-      <ArchitectureSection data={blueprint.architecture} />
-
-      <SecuritySection data={blueprint.security} />
-
-      <EdgeCasesSection data={blueprint.edge_cases} />
-
-      <ValidationSection data={blueprint.validation} />
 
 
     </div>
