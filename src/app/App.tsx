93642aa480366923c35e522b986449493e3cd26d
Niyati <niyatiagarwal2901@gmail.com>
2026-03-12 13:00:29 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing";
 import Clarification from "./pages/clarification";
 import Blueprint from "./pages/blueprint";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/clarification" element={<Clarification />} />
-        <Route path="/blueprint" element={<Blueprint />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/clarification" element={<Clarification />} />
+      <Route path="/blueprint" element={<Blueprint />} />
+    </Routes>
   );
 }
 
