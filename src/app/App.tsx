7558e1f3b928f8fe721867ff06eb7f0010cb4876
Niyati <niyatiagarwal2901@gmail.com>
2026-03-17 12:11:39 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./app/pages/Landing";
+import Landing from "./pages/landing";
 import Clarification from "./pages/clarification";
 import Blueprint from "./pages/blueprint";
 
